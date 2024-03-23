87cd7acc626f0ea3b8ba305aca74352e298144f7
David Bercovici <bercovici.david@gmail.com>
2024-03-23 15:34:56 -0500

    backgroundVideo added on homePage

--- content/pages/home.md
+++ content/pages/home.md
@@ -3,12 +3,14 @@ template: HomePage
 slug: ''
 title: 'La Cinta Corta'
 featuredImage: https://ucarecdn.com/df1c194b-935b-4fa8-9b2d-891dc0471d9c/Filmcrewteambgfilm.jpg
-# featuredImage: './images/banner2.png'
 subtitle: ''
 meta:
   description: Somos una productora cultural para Ecuador y el mundo
   title: Inicio
   image: 'https://ucarecdn.com/b3bbd594-4e2e-4961-865e-175e8dbd0b90/'
+video: 'https://ucarecdn.com/91fddfd0-eceb-4c49-bd7a-a99e89020034/videoBackground1.mp4'
+videoPoster: https://ucarecdn.com/69ba14a8-6481-4671-abb6-0e6f0d9c3e46/
+videoTitle: La Cinta Corta
 iconsImages:
   icon1: './images/video-camera.png'
   icon2: './images/video-camera.png'
--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -4,16 +4,22 @@ import ProjectGallery from '../components/ProjectsGallery'
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
+import BackgroundVideo from '../components/BackgroundVideo'
 
 // Export Template for use in CMS preview
-export const HomePageTemplate = ({ title, subtitle, featuredImage, body, banner2 }) => (
+export const HomePageTemplate = ({ title, subtitle, featuredImage, body, video, videoPoster, videoTitle,banner2 }) => (
   <main className="Home">
-    <PageHeader
+    {/* <PageHeader
       large      
       // title={title}
       subtitle={subtitle}
       backgroundImage={featuredImage}
-    />
+    /> */}
+    <section className='BackgroundVideo-section section'>
+      <BackgroundVideo poster={videoPoster} videoTitle={videoTitle}>
+      {video && <source src={video} type="video/mp4" />}
+      </BackgroundVideo>
+    </section>
 
     <div className="section">
       <div className="container">
@@ -85,6 +91,9 @@ export const pageQuery = graphql`
         title
         subtitle
         featuredImage
+        video
+        videoPoster
+        videoTitle
         banner2
       }
     }
