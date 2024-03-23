070ca86e2f1c1f33428bdf9f90c164c706f773fc
David Bercovici <bercovici.david@gmail.com>
2024-03-23 15:44:57 -0500

    change on BackgroundVideo compponent change value for mobileWidth less than 370

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -15,7 +15,7 @@ class BackgroundVideo extends Component {
   }
 
   updateDimensions() {
-    this.setState({ mobileWidth: window.innerWidth <= 375 })
+    this.setState({ mobileWidth: window.innerWidth <= 370 })
   }
 
   handelPlay() {
